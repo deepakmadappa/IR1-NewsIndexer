33e9c26c4c4aa3761f569362d9d4c77442d80992
Ramya Rao <ramyarao24@gmail.com>
2014-09-27 23:04:48 -0400

    Adding Analyzer chain

--- edu/buffalo/cse/irf14/analysis/AnalyzerChain.java
+++ edu/buffalo/cse/irf14/analysis/AnalyzerChain.java
@@ -52,13 +52,26 @@ public class AnalyzerChain implements Analyzer {
 	private TokenStream analyzeForAuthorOrg() throws TokenizerException{
 		TokenFilter accentFilter = TokenFilterFactory.getInstance().getFilterByType(TokenFilterType.ACCENT, mStream);
 		while(accentFilter.increment()) {}
-		return accentFilter.getStream();
+		TokenStream inter = accentFilter.getStream();
+
+		TokenFilter symFilter = TokenFilterFactory.getInstance().getFilterByType(TokenFilterType.SYMBOL, inter);
+		while(symFilter.increment()) {
+		}
+		inter = symFilter.getStream();
+		
+		TokenFilter spclFilter = TokenFilterFactory.getInstance().getFilterByType(TokenFilterType.SPECIALCHARS, inter);
+		while(spclFilter.increment()) {
+		}
+		return spclFilter.getStream();
+	
 	}
 	
 	private TokenStream analyzeForCategory() throws TokenizerException{
-		TokenFilter accentFilter = TokenFilterFactory.getInstance().getFilterByType(TokenFilterType.ACCENT, mStream);
-		while(accentFilter.increment()) {}
-		return accentFilter.getStream();
+		TokenFilter symFilter = TokenFilterFactory.getInstance().getFilterByType(TokenFilterType.SYMBOL, mStream);
+		while(symFilter.increment()) {
+			
+		}
+		return symFilter.getStream();
 
 	}
 	
@@ -101,27 +114,106 @@ public class AnalyzerChain implements Analyzer {
 		TokenFilter stemFilter = TokenFilterFactory.getInstance().getFilterByType(TokenFilterType.STEMMER, inter);
 		while(stemFilter.increment()) {
 		}
-		inter = stemFilter.getStream();
+		return stemFilter.getStream();
 	}
 	
-	private TokenStream analyzeForFileID() {
+	private TokenStream analyzeForFileID() throws TokenizerException {
+		
+		return mStream;
+
+	}
+	
+	private TokenStream analyzeForPlace() throws TokenizerException  {
+		
 		TokenFilter accentFilter = TokenFilterFactory.getInstance().getFilterByType(TokenFilterType.ACCENT, mStream);
 		while(accentFilter.increment()) {
-			
 		}
-		return accentFilter.getStream();
+		TokenStream inter = accentFilter.getStream();
+		
+		TokenFilter symFilter = TokenFilterFactory.getInstance().getFilterByType(TokenFilterType.SYMBOL, inter);
+		while(symFilter.increment()) {
+		}
+		inter = symFilter.getStream();
+		
+		TokenFilter spclFilter = TokenFilterFactory.getInstance().getFilterByType(TokenFilterType.SPECIALCHARS, inter);
+		while(spclFilter.increment()) {
+		}
+		return spclFilter.getStream();
 
 	}
 	
-	private TokenStream analyzeForPlace() {
-
+	private TokenStream analyzeForTitle() throws TokenizerException {
+		
+		TokenFilter accentFilter = TokenFilterFactory.getInstance().getFilterByType(TokenFilterType.ACCENT, mStream);
+		while(accentFilter.increment()) {
+		}
+		TokenStream inter = accentFilter.getStream();
+		
+		TokenFilter dateFilter = TokenFilterFactory.getInstance().getFilterByType(TokenFilterType.DATE, inter);
+		while(dateFilter.increment()) {
+		}
+		inter = dateFilter.getStream();
+		
+		TokenFilter numberFilter = TokenFilterFactory.getInstance().getFilterByType(TokenFilterType.NUMERIC, inter);
+		while(numberFilter.increment()) {
+		}
+		inter = numberFilter.getStream();
+		
+		TokenFilter capFilter = TokenFilterFactory.getInstance().getFilterByType(TokenFilterType.CAPITALIZATION, inter);
+		while(capFilter.increment()) {
+		}
+		inter = capFilter.getStream();
+		
+		TokenFilter symFilter = TokenFilterFactory.getInstance().getFilterByType(TokenFilterType.SYMBOL, inter);
+		while(symFilter.increment()) {
+		}
+		inter = symFilter.getStream();
+		
+		TokenFilter spclFilter = TokenFilterFactory.getInstance().getFilterByType(TokenFilterType.SPECIALCHARS, inter);
+		while(spclFilter.increment()) {
+		}
+		inter = spclFilter.getStream();
+		
+		TokenFilter stopFilter = TokenFilterFactory.getInstance().getFilterByType(TokenFilterType.STOPWORD, inter);
+		while(stopFilter.increment()) {
+		}
+		inter = stopFilter.getStream();
+		
+		TokenFilter stemFilter = TokenFilterFactory.getInstance().getFilterByType(TokenFilterType.STEMMER, inter);
+		while(stemFilter.increment()) {
+		}
+		return stemFilter.getStream();
+	
+		
 	}
 	
-	private TokenStream analyzeForTitle() {
+	private TokenStream analyzeForNewsDate() throws TokenizerException  {
+		
+		TokenFilter dateFilter = TokenFilterFactory.getInstance().getFilterByType(TokenFilterType.DATE, mStream);
+		while(dateFilter.increment()) {
+		}
+		TokenStream inter = dateFilter.getStream();
+		
+		TokenFilter numberFilter = TokenFilterFactory.getInstance().getFilterByType(TokenFilterType.NUMERIC, inter);
+		while(numberFilter.increment()) {
+		}
+		inter = numberFilter.getStream();
+		
 
-	}
-	
-	private TokenStream analyzeForNewsDate() {
+		TokenFilter capFilter = TokenFilterFactory.getInstance().getFilterByType(TokenFilterType.CAPITALIZATION, inter);
+		while(capFilter.increment()) {
+		}
+		inter = capFilter.getStream();
+		
+		TokenFilter symFilter = TokenFilterFactory.getInstance().getFilterByType(TokenFilterType.SYMBOL, inter);
+		while(symFilter.increment()) {
+		}
+		inter = symFilter.getStream();
+		
+		TokenFilter spclFilter = TokenFilterFactory.getInstance().getFilterByType(TokenFilterType.SPECIALCHARS, inter);
+		while(spclFilter.increment()) {
+		}
+		return spclFilter.getStream();
 
 	}
 }
