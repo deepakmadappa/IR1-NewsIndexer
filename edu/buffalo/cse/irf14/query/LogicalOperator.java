bec642e32717f2f8c39d316f6f6c655222c9abdf
deepakmadappa <deepumadappa@gmail.com>
2014-10-09 22:10:48 -0400

    Adding TreeNode

---
+++ edu/buffalo/cse/irf14/query/LogicalOperator.java
@@ -0,0 +1,5 @@
+package edu.buffalo.cse.irf14.query;
+
+public enum LogicalOperator {
+	AND, OR;
+}
---
+++ edu/buffalo/cse/irf14/query/TreeNode.java
@@ -0,0 +1,13 @@
+package edu.buffalo.cse.irf14.query;
+
+import edu.buffalo.cse.irf14.index.IndexType;
+
+public class TreeNode {
+	public TreeNode mLeftChild;
+	public TreeNode mRightChild;
+	public TreeNode mParent;
+	public LogicalOperator mOperator;
+	public IndexType mIndexType;
+	public String mSearchString;
+	public boolean mIsNot;
+}
