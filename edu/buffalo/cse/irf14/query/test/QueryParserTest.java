81545af8433de9939aa2b2950a2b579fba5f80fa
deepakmadappa <deepumadappa@gmail.com>
2014-10-19 17:52:54 -0400

    adding test file for queryParser

---
+++ edu/buffalo/cse/irf14/query/test/QueryParserTest.java
@@ -0,0 +1,23 @@
+package edu.buffalo.cse.irf14.query.test;
+
+import org.junit.Test;
+
+import edu.buffalo.cse.irf14.query.Query;
+import edu.buffalo.cse.irf14.query.QueryParser;
+
+import static org.junit.Assert.assertEquals;
+
+public class QueryParserTest {
+	@Test
+	public final void testQuery() {
+		Query query = null;
+		try {
+			query = QueryParser.parse("hello world", "AND");
+		}
+		catch (Exception ex) {
+			
+		}
+		String str = query.toString();
+		assertEquals("", str);
+	}
+}
